b8f12bb4940316d98c203cc9a7201090a953766f
Benjamin Brown <bpbrown@gmail.com>
2024-02-09 15:40:59 -0800

    Adds tracking of plan labels, from profile_labels branch of dedalus

--- plot_profiles.py
+++ plot_profiles.py
@@ -99,7 +99,7 @@ def plot_per_core_performance(stats_pdf_dict,
     ax_group = fig_group.add_subplot(1,1,1)
 
     group = {'linear algebra':["gssv", "apply_sparse", "superlu"],
-                  'MPI':["mpi4py.MPI", "fftw.fftw_wrappers.Transpose"],
+                  'MPI':["mpi4py.MPI", "fftw.fftw_wrappers.Transpose", "increment_plan", "decrement_plan"],
                   'FFT':["ifft", "_dct", "rfft", "unpack_rescale", 'repack_rescale', "forward", "backward"],
                   'arithmetic':["(operate)", "einsum", "arithmetic"],
                   'copy':["copyto", "gather_inputs", "gather_outputs", 'scatter_inputs', "scatter_outputs"],
