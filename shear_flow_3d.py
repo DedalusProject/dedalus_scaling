7665ce27e82761c7bceb46f6c2b101db0a9950b5
Benjamin Brown <bpbrown@gmail.com>
2024-02-13 20:22:26 -0800

    Brings shear_flow_3d into common driving patterns

--- shear_flow_3d.py
+++ shear_flow_3d.py
@@ -1,26 +1,71 @@
+"""
+Dedalus script simulating 3D triply-periodic shear flow.
+
+Usage:
+    shear_flow_3d.py [options]
+
+Options:
+    --nx=<nx>              Horizontal modes; default is nz
+    --ny=<ny>              Horizontal modes; default is nz
+    --nz=<nz>              Vertical modes   [default: 1024]
+
+    --niter=<iter>         Timing iterations [default: 100]
+    --nstart=<nstart>      Startup iterations [default: 10]
+
+    --dealias=<dealias>    Dealiasing [default: 1.5]
+
+    --mesh=<mesh>          Parallel decomposition mesh
+"""
+from mpi4py import MPI
 import numpy as np
 import dedalus.public as d3
 import logging
 logger = logging.getLogger(__name__)
 
+from docopt import docopt
+args = docopt(__doc__)
+
+comm = MPI.COMM_WORLD
+rank = comm.rank
+ncpu = comm.size
 
 # Parameters
 LL = 1
-NN = 1024
+nz = int(args['--nz'])
+if args['--nx']:
+    nx = int(args['--nx'])
+else:
+    nx = nz
+if args['--ny']:
+    ny = int(args['--ny'])
+else:
+    ny = nz
+
+mesh = args['--mesh']
+if mesh is not None:
+    mesh = mesh.split(',')
+    mesh = [int(mesh[0]), int(mesh[1])]
+else:
+    log2 = np.log2(ncpu)
+    if log2 == int(log2):
+        mesh = [int(2**np.ceil(log2/2)),int(2**np.floor(log2/2))]
+logger.info("running on processor mesh={}".format(mesh))
+
+
 Reynolds = 5e4
 Schmidt = 1
-dealias = 3/2
-stop_iteration = 200
+dealias = float(args['--dealias'])
+stop_iteration = int(float(args['--niter'])) + int(float(args['--nstart']))
 timestepper = d3.RK222
 timestep = 0.001
 dtype = np.float64
 
 # Bases
 coords = d3.CartesianCoordinates('x', 'y', 'z')
-dist = d3.Distributor(coords, dtype=dtype, mesh=(128,128))
-xbasis = d3.RealFourier(coords['x'], size=NN, bounds=(0, LL), dealias=dealias)
-ybasis = d3.RealFourier(coords['y'], size=NN, bounds=(0, LL), dealias=dealias)
-zbasis = d3.RealFourier(coords['z'], size=NN, bounds=(0, LL), dealias=dealias)
+dist = d3.Distributor(coords, dtype=dtype, mesh=mesh)
+xbasis = d3.RealFourier(coords['x'], size=nx, bounds=(0, LL), dealias=dealias)
+ybasis = d3.RealFourier(coords['y'], size=ny, bounds=(0, LL), dealias=dealias)
+zbasis = d3.RealFourier(coords['z'], size=nz, bounds=(0, LL), dealias=dealias)
 
 # Fields
 p = dist.Field(name='p', bases=(xbasis,ybasis,zbasis))
@@ -42,7 +87,7 @@ problem.add_equation("div(u) + tau_p = 0")
 problem.add_equation("integ(p) = 0") # Pressure gauge
 
 # Solver
-solver = problem.build_solver(timestepper, profile=True, profile_mode="full")
+solver = problem.build_solver(timestepper)
 solver.stop_sim_time = np.inf
 solver.stop_iteration = stop_iteration
 
@@ -55,16 +100,16 @@ s['g'] = u['g'][0]
 u['g'][1] += 0.1 * np.sin(2*np.pi*x/LL) * np.exp(-(z-0.5)**2/0.01)
 u['g'][1] += 0.1 * np.sin(2*np.pi*x/LL) * np.exp(-(z+0.5)**2/0.01)
 
+cadence = 100
 # Main loop
 try:
     logger.info('Starting main loop')
     while solver.proceed:
         solver.step(timestep)
-        if (solver.iteration-1) % 10 == 0:
+        if (solver.iteration-1) % cadence == 0:
             logger.info('Iteration=%i, Time=%e, dt=%e' %(solver.iteration, solver.sim_time, timestep))
 except:
     logger.error('Exception raised, triggering end of main loop.')
     raise
 finally:
     solver.log_stats()
-
