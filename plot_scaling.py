4f5264dee342c391a32580e5e811644060462412
Benjamin Brown <bpbrown@gmail.com>
2024-02-17 16:03:27 -0800

    Adds plot_timing_changes, which tracks how different group contributors (e.g., 'MPI', 'linear algebra') change for a problem at different core counts

--- plot_scaling.py
+++ plot_scaling.py
@@ -17,26 +17,37 @@ args = docopt(__doc__)
 
 data = {}
 for file in args['<files>']:
-    ncore = int(''.join(filter(str.isdigit, file)))
-    data[ncore] = {'cores' : [], 'speed' : []}
+    label = file.split('.')[0].split('_')[-1]
+    data[label] = {'cores' : [], 'speed' : []}
     with open(file, 'r') as f:
         for line in f:
             if "Speed" in line:
                 report = line.split('solvers')[1]
                 rr = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", report)
                 print(rr)
-                data[ncore]['cores'].append(int(rr[1]))
-                data[ncore]['speed'].append(float(rr[2]))
+                data[label]['cores'].append(int(rr[1]))
+                data[label]['speed'].append(float(rr[2]))
 
 data = dict(sorted(data.items()))
 
 fig, ax = plt.subplots(figsize=[6,6/1.6])
-for ncore in data:
-    ax.scatter(data[ncore]['cores'], data[ncore]['speed'], label=ncore, alpha=0.7)
+for label in data:
+    ax.scatter(data[label]['cores'], data[label]['speed'], label=label, alpha=0.7)
 ax.legend()
 ax.set_xlabel('N core')
-ax.set_ylabel('speed')
+ax.set_ylabel('speed = mode-stages/core-s')
 ax.set_xscale('log')
 ax.set_yscale('log')
 fig.tight_layout()
 fig.savefig('speed.png', dpi=300)
+
+fig, ax = plt.subplots(figsize=[6,6/1.6])
+for label in data:
+    ax.scatter(data[label]['cores'], np.array(data[label]['speed'])*np.array(data[label]['cores']), label=label, alpha=0.7)
+ax.legend()
+ax.set_xlabel('N core')
+ax.set_ylabel('speed*N core = mode-stages/s')
+ax.set_xscale('log')
+ax.set_yscale('log')
+fig.tight_layout()
+fig.savefig('work.png', dpi=300)
---
+++ plot_timing_changes.py
@@ -0,0 +1,225 @@
+"""
+Tool for ploting profiling results from Dedalus script runs.
+
+Usage:
+    plot_timing_changes.py <directory>... [options]
+
+Options:
+    --profile=<profile>    Profile data to plot (e.g., runtime, setup, warmup) [default: runtime]
+
+    --thresh=<thresh>      Threshold for trimming output, as a fraction of total time [default: 1e-4]
+
+    --max_profiles=<max>   Maximum number of profiles to output
+
+    --label=<label>        Optional label to add to output figures
+
+    --verbose              Display text verbose output to screen
+
+"""
+import os
+import pstats
+import numpy as np
+import pathlib
+import pickle
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+cmap = mpl.colormaps['tab10']
+debug = False
+
+
+def sort_by_total(joined_stat):
+    return sorted(joined_stat.items(), key=lambda kv: np.sum(kv[1]), reverse=True)
+
+def test_criteria(data):
+    return np.max(data)
+
+def clean_display(ax):
+    # from http://nbviewer.ipython.org/gist/anonymous/5357268
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+    ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_ticks_position('none')
+
+group = {'linear algebra':["gssv", "apply_sparse", "superlu", "linalg"],
+              'MPI':["mpi4py.MPI", "fftw.fftw_wrappers.Transpose", "fftw_RL_to_CL", "fftw_CL_to_RL", "localize_columns", "localize_rows", "RL_fftw", "CL_fftw"],
+              'FFT':["ifft", "_dct", "rfft", "unpack_rescale", 'repack_rescale', "forward", "backward"],
+              'arithmetic':["operate", "einsum", "arithmetic"],
+              'copy':["copyto", "gather_inputs", "gather_outputs", 'scatter_inputs', "scatter_outputs"],
+              'exclude': ["load_dynamic", "__init__", "<frozen", 'importlib']}
+
+def identify_categories(stats_pdf_dict,
+                              N_profiles=np.inf,
+                              thresh=0.02, verbose=False,
+                              dir=pathlib.Path('.')):
+
+    sorted_list = sort_by_total(stats_pdf_dict)
+
+    total_time = []
+    for item in tottime.items():
+        total_time.append(np.mean(item[1]))
+    total_time = np.sum(total_time)
+
+    group_data = {key:{} for key in group}
+    group_data['other'] = {}
+
+    N_cores = 0
+
+    for i_sort, (func, data) in enumerate(sorted_list):
+        if i_sort+1 == N_profiles:
+            break
+        #print(i_sort, func, data)
+        found_category = False
+        data = np.array(data)
+        for key in group:
+            tests = [item.lower() for item in group[key]]
+            if (any(item.lower() in func[0].lower() for item in group[key]) or any(item.lower() in func[2].lower() for item in group[key])) and test_criteria(data)/total_time > thresh:
+                if verbose and debug:
+                    print(f"found {key:s} call: {func[2]} in {tests} at {i_sort:d}")
+                group_data[key][func] = data
+                found_category = True
+        if not found_category and test_criteria(data)/total_time > thresh:
+             group_data['other'][func] = data
+        N_cores = max(N_cores, data.size)
+    N_profiles = 0
+    for key in group_data:
+        N_profiles += len(group_data[key])
+
+    if verbose and debug:
+        for func in group_data['exclude']:
+            print(f"found excluded call: {func[2]}, popping...")
+    excluded = group_data.pop('exclude', None)
+    if verbose and debug: print(excluded)
+
+    timings = {key:{} for key in group_data}
+    subtimings = {key:{} for key in group_data}
+    for i_group, key in enumerate(group_data):
+        group_time = np.zeros(N_cores)
+        first_item = True
+        for func, data_list in group_data[key].items():
+            data = np.array(data_list)
+            N_missing = N_cores - data.size
+
+            if N_missing != 0:
+                if verbose:
+                    print("missing {:d} values; setting to zero".format(N_missing))
+                for i in range(N_missing):
+                    data_list.insert(N_missing*(i+1)-1, 0)
+                data = np.array(data_list)
+
+            group_time += data
+
+            if func[0] == '~':
+                title_string = func[2]
+            else:
+                title_string = "{:s}:{:d}:{:s}".format(*func)
+
+            timing_data_string = "{:8.2g} |{:8.2g} |{:8.2g}".format(np.min(data), np.mean(data), np.max(data))
+
+            if verbose:
+                if first_item:
+                    print(f'{key:>60s} :')
+                    first_item = False
+                print("{:60s} = {:s}".format(title_string, timing_data_string))
+
+            subtimings[key][func] = {}
+            subtimings[key][func]['min']=np.min(data)
+            subtimings[key][func]['max']=np.max(data)
+            subtimings[key][func]['mean']=np.mean(data)
+
+        timings[key]['min']=np.min(group_time)
+        timings[key]['max']=np.max(group_time)
+        timings[key]['mean']=np.mean(group_time)
+    return timings, subtimings, N_cores
+
+def read_database(file):
+    with (open(file, "rb")) as f:
+        data = pickle.load(f)
+
+    primcalls = data['primcalls']
+    totcalls = data['totcalls']
+    tottime = data['tottime']
+    cumtime = data['cumtime']
+
+    return primcalls, totcalls, tottime, cumtime
+
+
+if __name__ == "__main__":
+    from docopt import docopt
+    args = docopt(__doc__)
+
+    label = args['--label']
+    if label:
+        label = '_'+str(label)
+    else:
+        label = ''
+    verbose = args['--verbose']
+    N_profiles = args['--max_profiles']
+    if N_profiles:
+        N_profiles = int(N_profiles)
+    else:
+        N_profiles = np.inf
+
+    mean_time = {}
+    n_cores = []
+    total_times = []
+
+    first_pass = True
+    for directory in args['<directory>']:
+        if verbose:
+            print(f'opening {directory:s}')
+        dir = pathlib.Path(directory)
+        profiles_file = str(args['--profile'])+'_parallel.pickle'
+        primcalls, totcalls, tottime, cumtime = read_database(dir / profiles_file)
+
+        timings, subtimings, N_cores = identify_categories(tottime, thresh=float(args['--thresh']), N_profiles=N_profiles, verbose=verbose)
+
+        total_time=0
+        n_cores.append(N_cores)
+        for key in timings:
+            if first_pass:
+                mean_time[key]=[]
+            mean_time[key].append(timings[key]['mean'])
+            total_time += timings[key]['mean']
+        total_times.append(total_time)
+        first_pass = False
+
+    i_sort = np.argsort(np.array(n_cores))
+
+    n_cores = np.array(n_cores)[i_sort]
+    total_times = np.array(total_times)[i_sort]
+    for key in mean_time:
+        mean_time[key] = np.array(mean_time[key])[i_sort]
+
+    fig, ax = plt.subplots()
+    previous_data = np.zeros(len(n_cores))
+    for i, key in enumerate(mean_time):
+        data = mean_time[key]
+        ax.fill_between(n_cores, data+previous_data, y2=previous_data, label=key, color=cmap(i),step='mid')
+        previous_data += data
+    ax.set_xlabel('N cores')
+    ax.set_ylabel('time [sec]')
+    ax.set_xscale('log', base=2)
+    ax.set_ylim(0,np.max(previous_data))
+    ax.set_xlim(np.min(n_cores),np.max(n_cores))
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f'total_group_time{label:s}.png', dpi=300)
+
+    fig, ax = plt.subplots()
+    previous_data = np.zeros(len(n_cores))
+    for i, key in enumerate(mean_time):
+        data = mean_time[key]/total_times
+        ax.fill_between(n_cores, data+previous_data, y2=previous_data, label=key, color=cmap(i),step='mid')
+        previous_data += data
+    ax.set_xscale('log', base=2)
+    ax.set_ylim(0,1)
+    ax.set_xlim(np.min(n_cores),np.max(n_cores))
+    ax.set_xlabel('N cores')
+    ax.set_ylabel('%time')
+    ax.legend(loc='center right', fontsize='small', framealpha=0.7)
+    fig.tight_layout()
+    fig.savefig(f'percent_group_time{label:s}.png', dpi=300)
